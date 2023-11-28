ff03a0351b92d87143299b7900081ba11f2f5c7c
Juthi <minhazurrahmanasif40@gmail.com>
2023-11-28 11:08:33 +0600

    add resume

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import asif from "../asset/asif photo.png"
+import asif2 from "../asset/asif2.jpg"
 const Header = () => {
           return (
   <div>
@@ -8,7 +8,7 @@ const Header = () => {
   <div class="hero-content flex-col lg:flex-row-reverse ">
     <img className='w-96 rounded-full' data-aos="fade-down"
      data-aos-easing="linear"
-     data-aos-duration="1500" src={asif}  />
+     data-aos-duration="1500" src={asif2}  />
     <div  data-aos="fade-up"
      data-aos-easing="ease-out-cubic"
      data-aos-duration="2000">
@@ -16,7 +16,7 @@ const Header = () => {
       <h1 class="text-5xl font-bold text-white">I'm Minhazur</h1>
       <h1 class="text-5xl font-bold text-white">Rahman</h1>
       <p class="py-6 text-white">I am a Junior web Developer. I built things for the web</p>
-      <a href='https://drive.google.com/file/d/1HU9thBM4HV6S5-hR6AYpjfyToKYUroS9/view?usp=sharing'><button class="btn btn-white text-white">Download Resume</button></a>
+      <a href='https://docs.google.com/document/d/1TeXAtkplNASpH2kIrQbB4uz_dnE_OU_0DQ9l3HC5FMQ/edit?usp=sharing'><button class="btn btn-white text-white">Download Resume</button></a>
       <Link className='m-7 sm:mt-3 md:mt-3' to='/contact'><button class="btn btn-white text-white">contact me</button></Link>
     </div>
   </div>
